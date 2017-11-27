47b77275bd218535ffcdb064c2dde4577b52a01c
Umang Leekha <umangleekha3@gmail.com>
2017-11-27 19:06:41 +0530

    Add radon tweaks

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -50,6 +50,21 @@ LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.radon.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.radon.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= radon.sh
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/radon.sh
+include $(BUILD_PREBUILT)
+
 # Configuration scripts
 
 include $(CLEAR_VARS)
